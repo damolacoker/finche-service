566d301610b3632a162648dfb925078947ea870f
sodamolacoker <damolacoker@gmail.com>
2019-12-01 00:09:34 +0000

    updating pom

--- login-service/src/main/java/com/finche/login/LoginApplication.java
+++ login-service/src/main/java/com/finche/login/LoginApplication.java
@@ -2,11 +2,12 @@ package com.finche.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @EnableEurekaClient
 @EnableFeignClients
 public class LoginApplication {
